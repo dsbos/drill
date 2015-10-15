750dd871c0f917aa3ce82419bdf40373199e7b96
dbarclay <dbarclay@maprtech.com>
2015-10-14 17:40:26 -0700

    2288tmp:  PURGED temp. OK -> OK_NEW_SCHEMA.  [xxx]

--- exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/ScanBatch.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/ScanBatch.java
@@ -177,7 +177,6 @@ public class ScanBatch implements CloseableRecordBatch {
 
   @Override
   public IterOutcome next() {
-    System.err.println( "????: ScanBatch OK case set to return OK_NEW_SCHEMA instead" );
     if (done) {
       return IterOutcome.NONE;
     }
@@ -263,8 +262,7 @@ public class ScanBatch implements CloseableRecordBatch {
         schema = container.getSchema();
         return IterOutcome.OK_NEW_SCHEMA;
       } else {
-        System.err.println( "????: OK case returning OK_NEW_SCHEMA instead" );
-        return IterOutcome.OK_NEW_SCHEMA; // ?????? TEMPORARY; WAS:     IterOutcome.OK;
+        return IterOutcome.OK;
       }
     } catch (OutOfMemoryRuntimeException ex) {
       context.fail(UserException.memoryError(ex).build(logger));
