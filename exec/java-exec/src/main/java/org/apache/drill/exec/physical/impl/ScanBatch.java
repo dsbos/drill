772ed88878fd4f00f88eaefc6e14fb84769e1f80
dbarclay <dbarclay@maprtech.com>
2015-10-27 10:09:02 -0700

    2288:  Pt. 4 Core:  Fixed ScanBatch.next() to return OK_NEW_SCHEMA instead of NONE for certain case of zero-row source/reader with new schema.  [ScanBatch]

--- exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/ScanBatch.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/ScanBatch.java
@@ -189,6 +189,8 @@ public class ScanBatch implements CloseableRecordBatch {
             if (mutator.isNewSchema()) {
               container.buildSchema(SelectionVectorMode.NONE);
               schema = container.getSchema();
+
+              return IterOutcome.OK_NEW_SCHEMA;
             }
             return IterOutcome.NONE;
           }
