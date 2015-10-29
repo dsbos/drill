b838d3420196996d8ac736d6e97894585cd24d72
dbarclay <dbarclay@maprtech.com>
2015-10-28 23:18:05 -0700

    2288TMP: Commented out TEMP. printlns, show...  [ScanBatch]

--- exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/ScanBatch.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/ScanBatch.java
@@ -151,7 +151,7 @@ public class ScanBatch implements CloseableRecordBatch {
 
   @Override
   public int getRecordCount() {
-    System.err.println( "???: ScanBatch.getRecordCount() returning (recordCount): " + recordCount );
+    //??????System.err.println( "???: ScanBatch.getRecordCount() returning (recordCount): " + recordCount );
     return recordCount;
   }
 
